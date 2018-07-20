f99f9c106b256ce3254ea0e72d222187f25a9345
wodjafink <wodjafink@gmail.com>
2018-07-19 21:36:14 -0700

    Pointing API to something useful

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,25 +2,13 @@ import axios from 'axios'
 
 export default {
 
-  // Gets all users
-  getUsers: () => ( axios.get('/users') ),
+  startSearch: function(params) {
+    return axios.post("/api/search", params);
+  },
 
-  // Gets all conversations
-  getConversations: () => ( axios.get('/conversations') ),
+  getResults: function() {
+    return axios.get("/api/results");
+  }
 
-  // Gets the conversation with the given id
-  getConversation: id => ( axios.get(`/conversations/${ id }`) ),
-
-  // Saves a book to the database
-  createConversation: convo => ( axios.post('/conversations', convo) ),
-
-  // Saves a book to the database
-  saveMessage: msg => ( axios.post('/messages', msg) ),
-
-  signup: credentials => ( axios.post('/signup', credentials) ),
-
-  login: credentials => ( axios.post('/login', credentials) ),
-
-  checkForSession: credentials => ( axios.get('/session') ),
 
 }
